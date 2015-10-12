52bf47905103790351eea7e8f813f17fa3552ad8
Chris Johnson <johnch@uwec.edu>
2015-10-12 07:24:47 -0500

    Adds option for non-manual comparing.

--- src/org/twodee/speccheck/CompareFrame.java
+++ src/org/twodee/speccheck/CompareFrame.java
@@ -20,17 +20,21 @@ public class CompareFrame<E extends JComponent> extends JDialog {
   private JButton sameButton;
 
   public CompareFrame() {
+    this(true);
+  }
+
+  public CompareFrame(boolean isManual) {
     setLayout(new GridBagLayout());
     setModal(true);
 
     GridBagConstraints constraints = new GridBagConstraints();
-    
+
     constraints.gridx = 0;
     constraints.gridy = 0;
     constraints.gridwidth = GridBagConstraints.REMAINDER;
     label = new JLabel();
     add(label, constraints);
-    
+
     constraints.gridwidth = 1;
     ++constraints.gridy;
     constraints.weightx = 1.0;
@@ -50,45 +54,65 @@ public class CompareFrame<E extends JComponent> extends JDialog {
     theirScroller = new JScrollPane();
     ++constraints.gridx;
     add(theirScroller, constraints);
-    
-    constraints.weighty = 0.0;
-    constraints.gridx = 0;
+
     ++constraints.gridy;
-    JButton button = new JButton("Different");
-    class PassFailListener implements ActionListener {
-      private boolean isPass;
-      public PassFailListener(boolean isPass) {
-        this.isPass = isPass;
+    if (isManual) {
+      constraints.weighty = 0.0;
+      constraints.gridx = 0;
+      JButton button = new JButton("Different");
+      class PassFailListener implements ActionListener {
+        private boolean isPass;
+
+        public PassFailListener(boolean isPass) {
+          this.isPass = isPass;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent arg0) {
+          isSame = isPass;
+          setVisible(false);
+        }
       }
-      @Override
-      public void actionPerformed(ActionEvent arg0) {
-        isSame = isPass;
-        setVisible(false);
+      button.addActionListener(new PassFailListener(false));
+      add(button, constraints);
+
+      constraints.gridx = 1;
+      sameButton = new JButton("Same");
+      sameButton.addActionListener(new PassFailListener(true));
+      add(sameButton, constraints);
+    } else {
+      constraints.weighty = 0.0;
+      constraints.gridx = 0;
+      constraints.gridwidth = 2;
+      sameButton = new JButton("Okay");
+      class OkayListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent arg0) {
+          isSame = false;
+          setVisible(false);
+        }
       }
+      sameButton.addActionListener(new OkayListener());
+      add(sameButton, constraints);
     }
-    button.addActionListener(new PassFailListener(false));
-    add(button, constraints);
-    
-    constraints.gridx = 1;
-    sameButton = new JButton("Same");
-    sameButton.addActionListener(new PassFailListener(true));
-    add(sameButton, constraints);
 
     setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
   }
 
-  public CompareFrame<E> compare(String msg, E ours, E theirs) {
+  public CompareFrame<E> compare(String msg,
+                                 E ours,
+                                 E theirs) {
     label.setText(msg);
     ourScroller.setViewportView(ours);
     theirScroller.setViewportView(theirs);
     pack();
-    
+
     sameButton.requestFocusInWindow();
     setVisible(true);
 
     return this;
   }
-  
+
   public boolean isSame() {
     return isSame;
   }
