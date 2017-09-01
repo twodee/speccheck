72662498f21219845ff51853575e456c9de67623
Chris Johnson <crjjrc@gmail.com>
2017-09-01 08:11:00 -0500

    Add image and color compares.

--- src/org/twodee/speccheck/SpecCheckTestSuite.java
+++ src/org/twodee/speccheck/SpecCheckTestSuite.java
@@ -1,5 +1,9 @@
 package org.twodee.speccheck;
 
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 import java.io.InputStream;
@@ -27,6 +31,12 @@ public class SpecCheckTestSuite {
   public static void assertEquals(String message, int expected, int actual) {
     if (expected != actual) {
       throw new AssertionError(String.format("%s%n      This is what I expected: %d%n  This is what I actually got: %d", message, expected, actual));
+    }
+  }
+
+  public static void assertEquals(String message, Color expected, Color actual) {
+    if (!expected.equals(actual)) {
+      throw new AssertionError(String.format("%s%n      This is the RGBA color I expected: (%3d, %3d, %3d, %3d)%n  This is the RGBA color I actually got: (%3d, %3d, %3d, %3d)", message, expected.getRed(), expected.getGreen(), expected.getBlue(), expected.getAlpha(), actual.getRed(), actual.getGreen(), actual.getBlue(), actual.getAlpha()));
     }
   }
 
@@ -131,6 +141,32 @@ public class SpecCheckTestSuite {
       };
       if (!DialogUtilities.isChecked("Final Steps", messages)) {
         Assert.fail("Not all items on your final steps checklist have been completed.");
+      }
+    }
+  }
+
+  private static boolean equalColors(int expected, int actual, int tolerance) {
+    Color expectedColor = new Color(expected);
+    Color actualColor = new Color(actual);
+    return Math.abs(expectedColor.getRed() - actualColor.getRed()) <= tolerance &&
+           Math.abs(expectedColor.getGreen() - actualColor.getGreen()) <= tolerance &&
+           Math.abs(expectedColor.getBlue() - actualColor.getBlue()) <= tolerance;
+  }
+
+  public static void assertEquals(boolean isVisual, String method, BufferedImage expected, BufferedImage actual, int tolerance) {
+    assertEquals("Method " + method + " produced an image whose width was different than expected.", expected.getWidth(), actual.getWidth());
+    assertEquals("Method " + method + " produced an image whose height was different than expected.", expected.getHeight(), actual.getHeight());
+
+    for (int r = 0; r < expected.getHeight(); ++r) {
+      for (int c = 0; c < expected.getWidth(); ++c) {
+        if (!equalColors(expected.getRGB(c, r), actual.getRGB(c, r), tolerance)) {
+          String msg = "Method " + method + " produced an image whose pixel (" + c + ", " + r + ") was not the expected color.";
+          if (isVisual) {
+            CompareFrame<JLabel> comparer = new CompareFrame<JLabel>(false);
+            comparer.compare(msg, new JLabel(new ImageIcon(expected)), new JLabel(new ImageIcon(actual)));
+          }
+          assertEquals(msg, new Color(expected.getRGB(c, r)), new Color(actual.getRGB(c, r)));
+        }
       }
     }
   }
